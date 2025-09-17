1653a6df23387556e4e6566043063994cc2e6756
Rudra Modi <rudramodi80606@gmail.com>
2025-09-17 14:19:16 +0530

    New version

--- prims.c
+++ prims.c
@@ -1,74 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 100
-#define INF 999999
+#define INF 99999
 
-int adjMatrix[MAX][MAX];
-int visited[MAX];
+void prim(int cost[MAX][MAX], int n) {
+    int selected[MAX] = {0};
+    int i, j, u = 0, v = 0, min, edges = 0, total = 0;
 
-// Prim's Algorithm
-int primMST(int startVertex, int numVertices) {
-    int minCost = 0;
-    visited[startVertex] = 1;
+    selected[0] = 1;
 
-    for (int count = 0; count < numVertices - 1; count++) {
-        int minEdgeCost = INF;
-        int src = -1, dest = -1;
-
-        // Find the minimum edge from visited → unvisited
-        for (int i = 0; i < numVertices; i++) {
-            if (visited[i]) {
-                for (int j = 0; j < numVertices; j++) {
-                    if (!visited[j] && adjMatrix[i][j] > 0 && adjMatrix[i][j] < minEdgeCost) {
-                        minEdgeCost = adjMatrix[i][j];
-                        src = i;
-                        dest = j;
+    printf("\nEdges in Prim's MST:\n");
+    while (edges < n - 1) {
+        min = INF;
+        for (i = 0; i < n; i++) {
+            if (selected[i]) {
+                for (j = 0; j < n; j++) {
+                    if (!selected[j] && cost[i][j] < min) {
+                        min = cost[i][j];
+                        u = i;
+                        v = j;
                     }
                 }
             }
         }
 
-        if (src == -1 || dest == -1) {
-            printf("Graph is disconnected. MST not possible.\n");
-            return -1;
-        }
-
-        visited[dest] = 1;
-        minCost += minEdgeCost;
-        printf("Edge selected: (%d -> %d) with cost %d\n", src + 1, dest + 1, minEdgeCost);
+        printf("%d - %d : %d\n", u, v, cost[u][v]);
+        total += cost[u][v];
+        selected[v] = 1;
+        edges++;
     }
-
-    return minCost;
+    printf("Total cost of Prim's MST: %d\n", total);
 }
 
 int main() {
-    int numVertices, numEdges;
+    int n, i, j;
+    int cost[MAX][MAX];
+
     printf("Enter the number of vertices: ");
-    scanf("%d", &numVertices);
+    scanf("%d", &n);
 
-    printf("Enter the number of edges: ");
-    scanf("%d", &numEdges);
-
-    // Initialize adjacency matrix
-    for (int i = 0; i < numVertices; i++) {
-        visited[i] = 0;
-        for (int j = 0; j < numVertices; j++) {
-            adjMatrix[i][j] = 0;
+    printf("Enter the adjacency matrix (Use %d for no edge):\n", INF);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
         }
     }
 
-    printf("Enter the edges (src dest weight) [Vertices numbered from 1 to %d]:\n", numVertices);
-    for (int i = 0; i < numEdges; i++) {
-        int src, dest, weight;
-        scanf("%d %d %d", &src, &dest, &weight);
-        src--; dest--; // convert to 0-based
-        adjMatrix[src][dest] = weight;
-        adjMatrix[dest][src] = weight;
-    }
-
-    printf("\n--------------- Prim's Algorithm ---------------\n");
-    int totalPrimCost = primMST(0, numVertices);
-    if (totalPrimCost != -1)
-        printf("Total cost using Prim's algorithm: %d\n", totalPrimCost);
+    prim(cost, n);
 
     return 0;
 }
